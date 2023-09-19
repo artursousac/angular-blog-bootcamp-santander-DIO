51a08609313fb6690aaffc8f9b24ca0e20d27995
Artur Cavalcante <artursousac@hotmail.com>
2023-09-19 12:08:07 -0300

    finalizado blog utilizando componentes no angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO",
+    "description": "Marvel anuncia novo filme do nome de ferro, confira!",
+    "photoCover": "https://i.pinimg.com/1200x/e3/02/c7/e302c7735be9b05571fd1df1868464b4.jpg"
+  },
+
+  {
+    "id":"2",
+    "title": "Nova série anunciada",
+    "description": "Marvel anuncia novo filme dos guardiões da galáxia",
+    "photoCover": "https://media.seudinheiro.com/uploads/2023/02/Guardians-of-the-Galaxy-Vol.-3-715x402.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Novo Filme Pantera Negra",
+    "description": "Marvel anuncia novo filme do Pantera Negra",
+    "photoCover": "https://mundonegro.inf.br/wp-content/uploads/2021/06/cropped-Pantera-Negra-1-1024x576.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "Novo Filme X-Men",
+    "description": "Marvel anuncia novo filme do X-Men",
+    "photoCover": "https://br.web.img2.acsta.net/pictures/14/05/08/22/33/194334.jpg"
+  }
+]
